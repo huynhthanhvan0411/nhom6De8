8056ed4b9fa0001db53f0956b0732b988a330ac5
thanhvan <huynhthanhvan0411@gmail.com>
2023-10-03 08:49:15 +0700

    thiii

--- mau.py
+++ mau.py
@@ -17,4 +17,14 @@ df = spark.createDataFrame(data, columns)
 # Tính tổng của cột 'age' cho những người có tuổi bằng 25
 sum_age = df.filter(df.age == 25).agg({"age": "sum"}).collect()[0][0]
 
+# Sắp xếp tăng dần theo cột 'age'
+sorted_df = df.orderBy("age")
+
+# Sắp xếp giảm dần theo cột 'age'
+# sorted_df = df.orderBy("age", ascending=False)
 print("Tổng của cột 'age' cho những người có tuổi bằng 25 là:", sum_age)
+print("Dữ liệu sau khi sắp xếp tăng dần theo cột 'age':")
+sorted_df.show()
+# Đếm số dòng có giá trị "very good" và "amazing" trong cột "Status"
+count_very_good = cau8.filter(cau8["Status"] == "very good").count()
+count_amazing = cau8.filter(cau8["Status"] == "amazing").count()
